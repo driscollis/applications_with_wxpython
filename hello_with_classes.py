e62523c6bb193cd3211760826869c28da5f9752b
Mike Driscoll <mike@pythonlibrary.org>
2018-11-09 08:20:52 -0600

    Hello world using classes

---
+++ hello_with_classes.py
@@ -0,0 +1,16 @@
+# hello_with_classes.py
+
+import wx
+
+
+class MyFrame(wx.Frame):
+    
+    def __init__(self):
+        wx.Frame.__init__(self, None, title='Hello World')
+        self.Show()
+
+
+if __name__ == '__main__':
+    app = wx.App(redirect=False)
+    frame = MyFrame()
+    app.MainLoop()
