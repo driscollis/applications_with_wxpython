f9236f035c7b9363638ae90287996f9357ae7143
Mike Driscoll <mike@pythonlibrary.org>
2018-11-09 08:21:08 -0600

    Use super() instead of regular __init__

---
+++ hello_with_classes_super.py
@@ -0,0 +1,16 @@
+# hello_with_classes_super.py
+
+import wx
+
+
+class MyFrame(wx.Frame):
+    
+    def __init__(self):
+        super(MyFrame, self).__init__(None, title='Hello World')
+        self.Show()
+
+
+if __name__ == '__main__':
+    app = wx.App(redirect=False)
+    frame = MyFrame()
+    app.MainLoop()
