50508b87baa5c5a7a2b43f84cbe97df5c9c29fd1
Mike Driscoll <mike@pythonlibrary.org>
2018-11-16 16:33:28 -0600

    Added image viewer folder example

---
+++ chapter3_updating_image_viewer/image_viewer_folder.py
@@ -0,0 +1,82 @@
+# image_viewer_folder.py
+
+import glob
+import os
+import wx
+
+class ImagePanel(wx.Panel):
+
+    def __init__(self, parent):
+        super(ImagePanel, self).__init__(parent)
+        self.max_size = 240
+        self.photos = []
+
+        img = wx.Image(240,240)
+        self.image_ctrl = wx.StaticBitmap(
+            self, bitmap=wx.Bitmap(img))
+    
+    def update_photo(self, image):
+        """
+        Update the currently shown photo
+        """
+        img = wx.Image(image, wx.BITMAP_TYPE_ANY)
+        # scale the image, preserving the aspect ratio
+        W = img.GetWidth()
+        H = img.GetHeight()
+        if W > H:
+            NewW = self.max_size
+            NewH = self.max_size * H / W
+        else:
+            NewH = self.max_size
+            NewW = self.max_size * W / H
+        img = img.Scale(NewW, NewH)
+    
+        self.image_ctrl.SetBitmap(wx.Bitmap(img))
+        self.Refresh()
+        
+
+class MainFrame(wx.Frame):
+
+    def __init__(self):
+        super(MainFrame, self).__init__(None, title='Image Viewer')
+        self.panel = ImagePanel(self)
+        self.create_toolbar()
+        self.Show()
+        
+    def create_toolbar(self):
+        """
+        Create a toolbar
+        """
+        self.toolbar = self.CreateToolBar()
+        self.toolbar.SetToolBitmapSize((16,16))
+        
+        open_ico = wx.ArtProvider.GetBitmap(
+            wx.ART_FILE_OPEN, wx.ART_TOOLBAR, (16,16))
+        openTool = self.toolbar.AddTool(
+            wx.ID_ANY, "Open", open_ico, "Open an Image Directory")
+        self.Bind(wx.EVT_MENU, self.on_open_directory, openTool)
+    
+        self.toolbar.Realize()
+        
+    def on_open_directory(self, event):
+        """
+        Open a directory dialog
+        """
+        dlg = wx.DirDialog(self, "Choose a directory",
+                                   style=wx.DD_DEFAULT_STYLE)
+         
+        if dlg.ShowModal() == wx.ID_OK:
+            self.folderPath = dlg.GetPath()
+            
+            photos = glob.glob(os.path.join(self.folderPath, '*.jpg'))
+            self.panel.photos = photos
+            if photos:
+                self.panel.update_photo(photos[0])
+            
+        dlg.Destroy()
+
+
+if __name__ == '__main__':
+    app = wx.App(redirect=False)
+    frame = MainFrame()
+    app.MainLoop()
