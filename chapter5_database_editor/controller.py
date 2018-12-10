7fb143306045d0571857a874aff978111cfbdc30
Mike Driscoll <mike@pythonlibrary.org>
2018-12-10 16:14:49 -0600

    updated the controller to be more robust

--- chapter5_database_editor/controller.py
+++ chapter5_database_editor/controller.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
  
 
-def add_record(data):
+def add_record(session, data):
     """
     Data should be a tuple of two dictionaries in the following format:
  
@@ -22,12 +22,9 @@ def add_record(data):
     author.first_name = data["author"]["first_name"]
     author.last_name = data["author"]["last_name"]
     book.person = author
- 
-    # connect to session and commit data to database
-    session = connectToDatabase()
+    
     session.add(book)
     session.commit()
-    session.close()
 
 
 def connect_to_database():
@@ -57,23 +54,20 @@ def convert_results(results):
     return books
 
 
-def delete_record(idNum):
+def delete_record(session, id_num):
     """
     Delete a record from the database
     """
-    session = connectToDatabase()
-    record = session.query(Book).filter_by(id=idNum).one()
+    record = session.query(Book).filter_by(id=id_num).one()
     session.delete(record)
     session.commit()
-    session.close()
 
 
-def edit_record(idNum, row):
+def edit_record(session, id_num, row):
     """
     Edit a record
     """
-    session = connectToDatabase()
-    record = session.query(Book).filter_by(id=idNum).one()
+    record = session.query(Book).filter_by(id=id_num).one()
     record.title = row["title"]
     record.person.first_name = row["first_name"]
     record.person.last_name = row["last_name"]
@@ -81,26 +75,22 @@ def edit_record(idNum, row):
     record.publisher = row["publisher"]
     session.add(record)
     session.commit()
-    session.close()
 
 
-def get_all_records():
+def get_all_records(session):
     """
     Get all records and return them
     """
-    session = connectToDatabase()
     result = session.query(Book).all()
     books = convertResults(result)
-    session.close()
     return books
  
 
-def search_records(filterChoice, keyword):
+def search_records(session, filterChoice, keyword):
     """
     Searches the database based on the filter chosen and the keyword
     given by the user
     """
-    session = connectToDatabase()
     if filterChoice == "Author":
         qry = session.query(Person)
         result = qry.filter(Person.first_name.contains('%s' % keyword)).all()
@@ -121,3 +111,7 @@ def search_records(filterChoice, keyword):
     books = convertResults(result)
     
     return books
+
+def setup_database():
+    """"""
+    metadata.create_all()
