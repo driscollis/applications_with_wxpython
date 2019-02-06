2eb427092eafba8415d79dd27d0802511f78f63b
Mike Driscoll <mike@pythonlibrary.org>
2019-02-06 15:55:51 -0600

    Add second version that can tar multiple paths

---
+++ chapter7_archiver/cli/archiver_v2.py
@@ -0,0 +1,50 @@
+import argparse
+import pathlib
+import tarfile
+import textwrap
+
+import controller
+
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Create a tar file',
+        epilog=textwrap.dedent(
+        '''
+        Example Usage:
+            archiver.py -t input_path
+            archiver.py --tar input_path -o output_path
+        ''')
+    )
+    parser.add_argument('-t', '--tar', 
+                        help='Create a tar file from the input path',
+                        required=True, action='store', nargs='+',
+                        dest='input_path')
+    parser.add_argument('-o', '--output',
+                        help='Output path', 
+                        action='store', 
+                        required=True,
+                        dest='output')
+    return parser.parse_args()
+
+
+def get_paths(paths):
+    path_objs = []
+    for path in paths:
+        path_objs.append(pathlib.Path(path))
+    return path_objs
+
+
+def main():
+    args = get_args()
+    if args.output:
+        output = pathlib.Path(args.output)
+        input_path = get_paths(args.input_path)
+    else:
+        output = pathlib.Path(f'{temp}.tar')
+        input_path = get_paths(args.input_path)
+    controller.create_tar(output, archive_objects=input_path)
+    print(f'Created tarball from {input_path} to {output}')
+    
+if __name__ == '__main__':
+    main()
