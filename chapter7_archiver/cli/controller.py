9c76a26bd9fd824b3f41402b3b6f044dbd7a8d96
Mike Driscoll <mike@pythonlibrary.org>
2019-02-06 14:30:07 -0600

    Added initial CLI archiver

---
+++ chapter7_archiver/cli/controller.py
@@ -0,0 +1,11 @@
+import os
+import tarfile
+
+from pathlib import Path
+
+
+def create_tar(path, archive_objects):
+    with tarfile.open(path, 'w') as tar:
+        for archive_object in archive_objects:
+            tar.add(archive_object, 
+                    arcname=archive_object.name)
