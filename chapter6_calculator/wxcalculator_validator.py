c9cb695318b1eb1a64b358f0613c1770a5f44c6a
Mike Driscoll <mike@pythonlibrary.org>
2019-03-22 13:24:02 -0500

    Fix bugs in one of the calculator examples

--- chapter6_calculator/wxcalculator_validator.py
+++ chapter6_calculator/wxcalculator_validator.py
@@ -1,10 +1,44 @@
-# wxcalculator_key_events.py
+# wxcalculator_validator.py
 
+import string
 import wx
 
 
+class CharValidator(wx.PyValidator):
+    '''
+    Validates data as it is entered into the text controls.
+    '''
+
+    def __init__(self, flag):
+        wx.PyValidator.__init__(self)
+        self.flag = flag
+        self.Bind(wx.EVT_CHAR, self.OnChar)
+
+    def Clone(self):
+        '''Required Validator method'''
+        return CharValidator(self.flag)
+
+    def Validate(self, win):
+        return True
+
+    def TransferToWindow(self):
+        return True
+
+    def TransferFromWindow(self):
+        return True
+
+    def OnChar(self, event):
+        keycode = int(event.GetKeyCode())
+        if keycode < 256:
+            key = chr(keycode)
+            if self.flag == 'no-alpha' and key in string.ascii_letters:
+                return
+            if self.flag == 'no-digit' and key in string.digits:
+                return
+        event.Skip()
+
 class CalcPanel(wx.Panel):
-    
+
     def __init__(self, parent):
         super().__init__(parent)
         self.last_button_pressed = None
@@ -12,22 +46,23 @@ class CalcPanel(wx.Panel):
                           ['4', '5', '6', '*'],
                           ['1', '2', '3', '-'],
                           ['.', '0', '', '+']]
-        self.on_key_called = False
+
         self.empty = True
         self.create_ui()
-        
+
     def create_ui(self):
         main_sizer = wx.BoxSizer(wx.VERTICAL)
         font = wx.Font(12, wx.MODERN, wx.NORMAL, wx.NORMAL)
-        
-        self.solution = wx.TextCtrl(self, style=wx.TE_RIGHT)
+
+        # Replace Disable() with a Validator
+        self.solution = wx.TextCtrl(self, style=wx.TE_RIGHT,
+                                    validator=CharValidator('no-alpha'))
         self.solution.SetFont(font)
-        # Replace Disable() with Bind()
-        self.solution.Bind(wx.EVT_TEXT, self.on_key)
+
         main_sizer.Add(self.solution, 0, wx.EXPAND|wx.ALL, 5)
         self.running_total = wx.StaticText(self)
         main_sizer.Add(self.running_total, 0, wx.ALIGN_RIGHT)
-        
+
         for label_list in self.whitelist:
             btn_sizer = wx.BoxSizer()
             for label in label_list:
@@ -35,50 +70,38 @@ class CalcPanel(wx.Panel):
                 btn_sizer.Add(button, 1, wx.ALIGN_CENTER|wx.EXPAND, 0)
                 button.Bind(wx.EVT_BUTTON, self.on_calculate)
             main_sizer.Add(btn_sizer, 1, wx.ALIGN_CENTER|wx.EXPAND)
-        
+
         equals_btn = wx.Button(self, label='=')
         equals_btn.Bind(wx.EVT_BUTTON, self.on_total)
         main_sizer.Add(equals_btn, 0, wx.EXPAND|wx.ALL, 3)
-        
+
         clear_btn = wx.Button(self, label='Clear')
         clear_btn.Bind(wx.EVT_BUTTON, self.on_clear)
         main_sizer.Add(clear_btn, 0, wx.EXPAND|wx.ALL, 3)
-        
+
         self.SetSizer(main_sizer)
-        
+
     def on_calculate(self, event):
         btn = event.GetEventObject()
         label = btn.GetLabel()
-        self.on_key_called = True
         self.update_equation(label)
-        
+
     def on_clear(self, event):
         self.solution.Clear()
         self.running_total.SetLabel('')
         self.empty = True
         self.solution.SetFocus()
-        
-    def on_key(self, event):
-        if self.on_key_called:
-            self.on_key_called = False
-            return
-        
-        key = event.GetString()
-        self.on_key_called = True
-        
-        if key in self.whitelist:
-            self.update_equation(key)
-    
+
     def on_total(self, event):
         solution = self.update_solution()
         if solution:
             self.solution.SetValue(solution)
             self.running_total.SetLabel('')
-    
+
     def update_equation(self, text):
         operators = ['/', '*', '-', '+']
         current_equation = self.solution.GetValue()
-        
+
         if text not in operators:
             if self.last_button_pressed in operators:
                 self.solution.SetValue(current_equation + ' ' + text)
@@ -90,15 +113,15 @@ class CalcPanel(wx.Panel):
         elif text in operators and current_equation != '' \
              and self.last_button_pressed not in operators:
             self.solution.SetValue(current_equation + ' ' + text)
-        
+
         self.last_button_pressed = text
         self.solution.SetInsertionPoint(-1)
-        
+
         for item in operators:
             if item in self.solution.GetValue():
                 self.update_solution()
                 break
-        
+
     def update_solution(self):
         try:
             current_solution = str(eval(self.solution.GetValue()))
@@ -109,10 +132,10 @@ class CalcPanel(wx.Panel):
             self.solution.SetValue('ZeroDivisionError')
         except:
             pass
-        
+
 
 class CalcFrame(wx.Frame):
-    
+
     def __init__(self):
         super().__init__(
             None, title="wxCalculator",
