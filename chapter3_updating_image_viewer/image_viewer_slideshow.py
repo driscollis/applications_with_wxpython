34d1bdebc34662d36b35008ee0261a864e5e613a
Mike Driscoll <mike@pythonlibrary.org>
2019-03-28 09:59:32 -0500

    Fixed some formatting issues

--- chapter3_updating_image_viewer/image_viewer_slideshow.py
+++ chapter3_updating_image_viewer/image_viewer_slideshow.py
@@ -13,35 +13,35 @@ class ImagePanel(wx.Panel):
         self.current_photo = 0
         self.total_photos = 0
         self.layout()
-        
+
         self.slideshow_timer = wx.Timer(self)
         self.Bind(wx.EVT_TIMER, self.on_next, self.slideshow_timer)
-        
+
     def layout(self):
         """
         Layout the widgets on the panel
         """
-    
+
         self.main_sizer = wx.BoxSizer(wx.VERTICAL)
         btn_sizer = wx.BoxSizer(wx.HORIZONTAL)
-    
+
         img = wx.Image(self.max_size, self.max_size)
-        self.image_ctrl = wx.StaticBitmap(self, wx.ID_ANY, 
+        self.image_ctrl = wx.StaticBitmap(self, wx.ID_ANY,
                                              wx.Bitmap(img))
         self.main_sizer.Add(self.image_ctrl, 0, wx.ALL|wx.CENTER, 5)
         self.image_label = wx.StaticText(self, label="")
         self.main_sizer.Add(self.image_label, 0, wx.ALL|wx.CENTER, 5)
-    
+
         btn_data = [("Previous", btn_sizer, self.on_previous),
                     ("Slide Show", btn_sizer, self.on_slideshow),
                     ("Next", btn_sizer, self.on_next)]
         for data in btn_data:
             label, sizer, handler = data
             self.btn_builder(label, sizer, handler)
-    
+
         self.main_sizer.Add(btn_sizer, 0, wx.CENTER)
         self.SetSizer(self.main_sizer)
-        
+
     def btn_builder(self, label, sizer, handler):
         """
         Builds a button, binds it to an event handler and adds it to a sizer
@@ -49,33 +49,33 @@ class ImagePanel(wx.Panel):
         btn = wx.Button(self, label=label)
         btn.Bind(wx.EVT_BUTTON, handler)
         sizer.Add(btn, 0, wx.ALL|wx.CENTER, 5)
-        
+
     def on_next(self, event):
         """
         Loads the next picture in the directory
         """
         if not self.photos:
             return
-        
+
         if self.current_photo == self.total_photos - 1:
             self.current_photo = 0
         else:
             self.current_photo += 1
         self.update_photo(self.photos[self.current_photo])
-    
+
     def on_previous(self, event):
         """
         Displays the previous picture in the directory
         """
         if not self.photos:
             return
-        
+
         if self.current_photo == 0:
             self.current_photo = self.total_photos - 1
         else:
             self.current_photo -= 1
         self.update_photo(self.photos[self.current_photo])
-    
+
     def on_slideshow(self, event):
         """
         Starts and stops the slideshow
@@ -88,7 +88,7 @@ class ImagePanel(wx.Panel):
         else:
             self.slideshow_timer.Stop()
             btn.SetLabel("Slide Show")
-            
+
     def update_photo(self, image):
         """
         Update the currently shown photo
@@ -104,10 +104,10 @@ class ImagePanel(wx.Panel):
             NewH = self.max_size
             NewW = self.max_size * W / H
         img = img.Scale(NewW, NewH)
-    
+
         self.image_ctrl.SetBitmap(wx.Bitmap(img))
         self.Refresh()
-        
+
     def reset(self):
         img = wx.Image(self.max_size,
                        self.max_size)
@@ -115,42 +115,42 @@ class ImagePanel(wx.Panel):
         self.image_ctrl.SetBitmap(bmp)
         self.current_photo = 0
         self.photos = []
-        
+
 
 class MainFrame(wx.Frame):
 
     def __init__(self):
         super().__init__(None, title='Image Viewer',
-                                        size=(400, 400))
+                         size=(400, 400))
         self.panel = ImagePanel(self)
         self.create_toolbar()
         self.Show()
-        
+
     def create_toolbar(self):
         """
         Create a toolbar
         """
         self.toolbar = self.CreateToolBar()
         self.toolbar.SetToolBitmapSize((16,16))
-        
+
         open_ico = wx.ArtProvider.GetBitmap(
             wx.ART_FILE_OPEN, wx.ART_TOOLBAR, (16,16))
         openTool = self.toolbar.AddTool(
             wx.ID_ANY, "Open", open_ico, "Open an Image Directory")
         self.Bind(wx.EVT_MENU, self.on_open_directory, openTool)
-    
+
         self.toolbar.Realize()
-        
+
     def on_open_directory(self, event):
         """
         Open a directory dialog
         """
         with wx.DirDialog(self, "Choose a directory",
                           style=wx.DD_DEFAULT_STYLE) as dlg:
-         
+
             if dlg.ShowModal() == wx.ID_OK:
                 self.folderPath = dlg.GetPath()
-                
+
                 photos = glob.glob(os.path.join(self.folderPath, '*.jpg'))
                 self.panel.photos = photos
                 if photos:
