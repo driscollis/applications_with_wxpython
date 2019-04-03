fa9bb2f47bb42b2e9c274b9fd803539f61628616
Mike Driscoll <mike@pythonlibrary.org>
2019-04-03 15:30:26 -0500

    Added validator

--- chapter10_pdf_split_merge/version_1/split_panel.py
+++ chapter10_pdf_split_merge/version_1/split_panel.py
@@ -1,5 +1,44 @@
 import os
+import string
 import wx
+
+from PyPDF2 import PdfFileReader, PdfFileWriter
+
+wildcard = "PDFs (*.pdf)|*.pdf"
+
+
+class CharValidator(wx.PyValidator):
+    '''
+    Validates data as it is entered into the text controls.
+    '''
+
+    def __init__(self, flag):
+        wx.PyValidator.__init__(self)
+        self.flag = flag
+        self.Bind(wx.EVT_CHAR, self.OnChar)
+
+    def Clone(self):
+        '''Required Validator method'''
+        return CharValidator(self.flag)
+
+    def Validate(self, win):
+        return True
+
+    def TransferToWindow(self):
+        return True
+
+    def TransferFromWindow(self):
+        return True
+
+    def OnChar(self, event):
+        keycode = int(event.GetKeyCode())
+        if keycode < 256:
+            key = chr(keycode)
+            if self.flag == 'no-alpha' and key in string.ascii_letters:
+                return
+            if self.flag == 'no-digit' and key in string.digits:
+                return
+        event.Skip()
 
 
 class SplitPanel(wx.Panel):
@@ -16,6 +55,7 @@ class SplitPanel(wx.Panel):
         self.pdf_path = wx.TextCtrl(self, style=wx.TE_READONLY)
         row_sizer.Add(self.pdf_path, 1, wx.EXPAND | wx.ALL, 5)
         pdf_btn = wx.Button(self, label='Open PDF')
+        pdf_btn.Bind(wx.EVT_BUTTON, self.on_choose)
         row_sizer.Add(pdf_btn, 0, wx.ALL, 5)
         main_sizer.Add(row_sizer, 0, wx.EXPAND)
 
@@ -24,7 +64,8 @@ class SplitPanel(wx.Panel):
         page_lbl = wx.StaticText(self, label='Pages:')
         page_lbl.SetFont(font)
         row_sizer.Add(page_lbl, 0, wx.ALL | wx.CENTER, 5)
-        self.pdf_split_options = wx.TextCtrl(self)
+        self.pdf_split_options = wx.TextCtrl(
+            self, validator=CharValidator('no-alpha'))
         row_sizer.Add(self.pdf_split_options, 0, wx.ALL, 5)
         main_sizer.Add(row_sizer)
 
@@ -44,9 +85,21 @@ class SplitPanel(wx.Panel):
         self.SetSizer(main_sizer)
 
     def on_choose(self, event):
-        pass
+        path = None
+        with wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir='~',
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.FD_OPEN | wx.FD_CHANGE_DIR
+            ) as dlg:
+            if dlg.ShowModal() == wx.ID_OK:
+                path = dlg.GetPath()
+        if path:
+            self.pdf_path.SetValue(path)
 
     def on_split(self, event):
+        output_path = None
         input_pdf = self.pdf_path.GetValue()
         split_options = self.pdf_split_options.GetValue()
         if not input_pdf:
@@ -75,7 +128,53 @@ class SplitPanel(wx.Panel):
                 dlg.ShowModal()
             return
 
-        self.split(input_pdf, split_options)
+        with wx.FileDialog(
+            self, message="Choose a file",
+            defaultDir='~',
+            defaultFile="",
+            wildcard=wildcard,
+            style=wx.FD_SAVE | wx.FD_CHANGE_DIR
+            ) as dlg:
+            if dlg.ShowModal() == wx.ID_OK:
+                output_path = dlg.GetPath()
 
-    def split(self, input_pdf, split_options):
-        pass
+        if output_path:
+            _, ext = os.path.splitext(output_path)
+            if '.pdf' not in ext.lower():
+                output_path = f'{output_path}.pdf'
+            split_options = split_options.strip()
+            self.split(input_pdf, output_path, split_options)
+
+    def split(self, input_pdf, output_path, split_options):
+        pdf = PdfFileReader(input_pdf)
+        pdf_writer = PdfFileWriter()
+        if ',' in split_options:
+            pages = split_options.split(',')
+            for page in pages:
+                pdf_writer.addPage(pdf.getPage(int(page)))
+        else:
+            page_begin, page_end = split_options.split('-')
+            page_begin = int(page_begin)
+            page_end = int(page_end)
+            if page_begin < 0 or page_begin == 1:
+                page_begin = 0
+            if page_begin > 1:
+                # Take off by one error into account
+                page_begin -= 1
+
+            for page in range(page_begin, page_end):
+                pdf_writer.addPage(pdf.getPage(page))
+
+        # Write PDF to disk
+        with open(output_path, 'wb') as out:
+            pdf_writer.write(out)
+
+        # Let user know that PDF is split
+        message = f'PDF split successfully to {output_path}'
+        with wx.MessageDialog(None, message=message,
+                              caption='Split Finished',
+                              style= wx.ICON_INFORMATION) as dlg:
+            dlg.ShowModal()
+
+
+
