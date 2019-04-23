930dc4206ebae3eb7a187b357c049598a75754a1
Mike Driscoll <mike@pythonlibrary.org>
2019-04-23 11:59:46 -0500

    rename method to a more accurate name

--- chapter1_intro_to_wx/event_hierarchy.py
+++ chapter1_intro_to_wx/event_hierarchy.py
@@ -4,23 +4,23 @@ import wx
 
 
 class MyPanel(wx.Panel):
-    
+
     def __init__(self, parent):
         super().__init__(parent)
-        
+
         button = wx.Button(self, label='Press Me')
-        self.Bind(wx.EVT_BUTTON, self.frame_button_handler, button)
+        self.Bind(wx.EVT_BUTTON, self.panel_button_handler, button)
         button.Bind(wx.EVT_BUTTON, self.on_button_press)
-        
-    def frame_button_handler(self, event):
-        print('frame_button_handler called')
-        
+
+    def panel_button_handler(self, event):
+        print('panel_button_handler called')
+
     def on_button_press(self, event):
         print('on_button_press called')
         event.Skip()
 
 class MyFrame(wx.Frame):
-    
+
     def __init__(self):
         super().__init__(None, title='Hello World')
         panel = MyPanel(self)
