610bbdf4c8175a93f14ff9f813ff5e5869d0c1ca
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-14 01:55:14 +0800

    Delete chapter7_archiver directory

--- chapter7_archiver/cli/controller.py
+++
@@ -1,9 +0,0 @@
-# controller.py
-
-import tarfile
-
-def create_tar(path, archive_objects):
-    with tarfile.open(path, 'w') as tar:
-        for archive_object in archive_objects:
-            tar.add(archive_object,
-                    arcname=archive_object.name)
