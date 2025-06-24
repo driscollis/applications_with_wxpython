5b91ada4a17e324cb676d0c8cd7b4202a3be075d
Mike Driscoll <mike@pythonlibrary.org>
2025-06-24 12:55:38 -0500

    Fix code error

--- chapter2_image_viewer/image_viewer_button_event.py
+++ chapter2_image_viewer/image_viewer_button_event.py
@@ -8,26 +8,26 @@ class ImagePanel(wx.Panel):
         super().__init__(parent)
 
         img = wx.Image(*image_size)
-        self.image_ctrl = wx.StaticBitmap(self, 
+        self.image_ctrl = wx.StaticBitmap(self,
                                           bitmap=wx.Bitmap(img))
-        
+
         browse_btn = wx.Button(self, label='Browse')
         browse_btn.Bind(wx.EVT_BUTTON, self.on_browse)
-        
+
         self.photo_txt = wx.TextCtrl(self, size=(200, -1))
 
         main_sizer = wx.BoxSizer(wx.VERTICAL)
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
-        
+
         main_sizer.Add(self.image_ctrl, 0, wx.ALL, 5)
         hsizer.Add(browse_btn, 0, wx.ALL, 5)
         hsizer.Add(self.photo_txt, 0, wx.ALL, 5)
         main_sizer.Add(hsizer, 0, wx.ALL, 5)
-        
+
         self.SetSizer(main_sizer)
         main_sizer.Fit(parent)
         self.Layout()
-        
+
     def on_browse(self, event):
         """
         Browse for an image file
@@ -36,7 +36,7 @@ class ImagePanel(wx.Panel):
         wildcard = "JPEG files (*.jpg)|*.jpg"
         with wx.FileDialog(None, "Choose a file",
                            wildcard=wildcard,
-                           style=wx.ID_OPEN) as dialog:
+                           style=wx.FD_OPEN) as dialog:
             if dialog.ShowModal() == wx.ID_OK:
                 self.photo_txt.SetValue(dialog.GetPath())
 
